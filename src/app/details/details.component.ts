0bc3d4e9b60cf07fa32322e5df5661bb9c76bc06
nikakaladze <nikakaladze89@gmail.com>
2023-12-22 16:38:03 +0400

    working architectture and some design

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      details Works! {{ housingLocationId }}
+    </p>
+  `,
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
+
+
+  constructor(){
+    this.housingLocationId = Number(this.route.snapshot.params[""])
+  }
+
+}
